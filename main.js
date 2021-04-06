3f841eadfe67be09cdf0436bd50f2cad7883991a
Saulnier <Saulnier412@gmail.com>
2021-04-06 09:30:58 -0600

    add files

---
+++ main.js
@@ -0,0 +1,15 @@
+//user pick Rock Paper Scissor
+function userPlay() {
+
+}
+
+//randomly return Rock Paper Scissor
+
+function computerPlay() {
+
+}
+
+//compare userPlay with computerPlay
+function answer() {
+
+}
